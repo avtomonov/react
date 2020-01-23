9f543e15514daa98c081ea30ae8c025f2a506bca
Andrey Avtomonov <web@hu2.ru>
2020-01-23 18:04:23 +0300

    first commit

---
+++ itb-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  account: {
+    id: 'd17cf790c2b5895de63309af3d92f13a',
+    token: '25c9c56fbb4ed8cc2a91917400f6c6dc',
+    url: 'avtomonov.myinsales.ru',
+    http: true
+  },
+  theme: {
+    id: '1341151',
+    assetsSync: false,
+    assetsDomain: 'https://assets.insales.ru'
+  },
+  mixedComponents: {},
+  customTheme: {}
+};
---
+++ project/components/re/re.js
@@ -0,0 +1,29 @@
+
+var element = React.createElement('h1', null, 'hi');
+
+
+
+ReactDOM.render(element, document.getElementById('hello-root'));
+
+function Get_List_Product(handle, ArrCharacteristics){
+  if(handle == undefined){
+    var handle = 'all'
+  }
+  
+  if(ArrCharacteristics == undefined){
+    var ArrCharacteristics = [];
+  }
+  
+  $.post('/collection/'+ handle +'.json', {
+    characteristics: ArrCharacteristics
+  })
+  .done(function (products) {
+
+    var Product_List = React.createElement('ul', {"data-products-list":"true"}, );
+
+    ReactDOM.render(Product_List, document.getElementById('product-list'));
+    
+  })
+
+}
+
